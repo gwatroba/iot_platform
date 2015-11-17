009360a820a62393b09daf4b25b2bdc4a002878b
Griszmen <grzegorz.watroba@gmail.com>
2015-11-17 15:54:38 +0100

    Working v1 with HUB support

--- api_requests.py
+++ api_requests.py
@@ -22,30 +22,92 @@ def get_types(url, username, password):
         return [], get_resp
 
 
-def add_sensor(url, username, password, typeId, typeName, sensorName, bleId):
+def get_hub_id(url, username, password):
+
+    get_resp = request('GET', url + '/ca/api/devices?size=5000', verify=False, auth=HTTPBasicAuth(username, password))
+
+    if get_resp.status_code == 200:
+        json_data = json.loads(get_resp.text)
+        hub_id = False
+        for hub in json_data['content']:
+            if hub['type'] == 'ACTIVE_HUB':
+                hub_id = hub['id']
+        if bool(hub_id):
+            return hub_id
+        else:
+            return False
+
+
+def get_device_id(url, username, password, uuid):
+    get_resp = request('GET', url + '/ca/api/devices?size=5000', verify=False, auth=HTTPBasicAuth(username, password))
+
+    if get_resp.status_code == 200:
+        json_data = json.loads(get_resp.text)
+        device_id = False
+        for device in json_data['content']:
+            if device['uuid'] == uuid:
+                device_id = device['id']
+        if bool(device_id):
+            return device_id
+        else:
+            return False
+
+
+def add_to_hub(url, username, password, hub_id, device_id):
+
+    query = '["%s"]' % device_id
+    query_json = json.loads(query)
+    post_resp = request('POST', url + '/ca/api/devices/%s/topology' % hub_id, verify=False, auth=HTTPBasicAuth(username, password), json=query_json)
+    if post_resp.status_code == 200:
+        return post_resp
+    else:
+        return post_resp
+
+
+def add_sensor(url, username, password, type_id, type_name, sensor_name, ble_id):
 
     uuid_end = b2a_hex(urandom(6)).decode("utf-8")
     add_sensor_query = {
-    "typeId": typeId,
-    "type": typeName,
+    "typeId": type_id,
+    "type": type_name,
     "uuid": "88888888-8888-8888-8888-%s" % uuid_end,
-    "name": sensorName,
+    "name": sensor_name,
     "registrationKey": "regkey-" + ''.join(random.choice(string.digits) for _ in range(4)),
     "additionalIds": {
         "values": {
-            "CA_BLE_ID": bleId
+            "CA_BLE_ID": ble_id
         }
     }
     }
-
     post_resp = request('POST', url + '/ca/api/devices/', verify=False, auth=HTTPBasicAuth(username, password), json=add_sensor_query)
 
     if post_resp.status_code == 200:
         post_web = request('POST', url + '/ca/api/devices/pair/key/%s' % add_sensor_query['registrationKey'], verify=False, auth=HTTPBasicAuth(username, password))
-        return add_sensor_query, str(post_resp), str(post_web)
+        return add_sensor_query, post_resp, post_web, add_sensor_query['uuid']
     else:
-        return add_sensor_query, str(post_resp), "N/A"
+        return add_sensor_query, post_resp, "N/A"
+
+
+def add_hub(url, username, password, hub_name):
+    uuid_end = b2a_hex(urandom(6)).decode("utf-8")
+
+    add_hub_query = {
+    "typeId" : "55d1a6d5e4b010f33a027cfd",
+    "type" : "ACTIVE_HUB",
+    "uuid" : "88888888-8888-8888-8888-%s" % uuid_end,
+    "name" : hub_name,
+    "registrationKey" : "regkey-" + ''.join(random.choice(string.digits) for _ in range(4)),
+    "additionalIds" : {
+        "values": {
+        }
+    }
+    }
+    post_resp = request('POST', url + '/ca/api/devices/', verify=False, auth=HTTPBasicAuth(username, password), json=add_hub_query)
+
+    if post_resp.status_code == 200:
+        post_web = request('POST', url + '/ca/api/devices/pair/key/%s' % add_hub_query['registrationKey'], verify=False, auth=HTTPBasicAuth(username, password))
+        return add_hub_query, post_resp, post_web
+    else:
+        return add_hub_query, post_resp, "N/A"
 
 # BASE PROGRAM
-
-
--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.spinner import Spinner
 from kivy.uix.popup import Popup
+from kivy.uix.checkbox import CheckBox
 import api_requests
 
 
@@ -12,17 +13,18 @@ import api_requests
 
 layout = FloatLayout(size=(300, 300))
 url_label = Label(text="Base url:", font_size=15, size_hint=(0.05, 0.05), pos_hint={'x': 0.03, 'y': 0.85})
-url_text = TextInput(text="https://maja-iot.lodz2.comarch:10443", multiline=False, font_size=12, size_hint=(0.3, 0.05), pos_hint={'x': 0.12, 'y': 0.85}, write_tab=False)
+url_text = TextInput(text="", multiline=False, font_size=12, size_hint=(0.3, 0.05), pos_hint={'x': 0.12, 'y': 0.85}, write_tab=False)
 
 login_label = Label(text="Login:", font_size=15, size_hint=(0.05, 0.05), pos_hint={'x': 0.03, 'y': 0.77})
-login_text = TextInput(multiline=False, font_size=12, size_hint=(0.2, 0.05), pos_hint={'x': 0.12, 'y': 0.77}, write_tab=False)
+login_text = TextInput(text='', multiline=False, font_size=12, size_hint=(0.2, 0.05), pos_hint={'x': 0.12, 'y': 0.77}, write_tab=False)
 
 pass_label = Label(text="Password:", font_size=15, size_hint=(0.05, 0.05), pos_hint={'x': 0.03, 'y': 0.69})
-pass_text = TextInput(multiline=False, font_size=12, password=True, size_hint=(0.2, 0.05), pos_hint={'x': 0.12, 'y': 0.69})
+pass_text = TextInput(text='', multiline=False, font_size=12, password=True, size_hint=(0.2, 0.05), pos_hint={'x': 0.12, 'y': 0.69})
 
 button1 = Button(text="Get Types", font_size=15, size_hint=(0.12, 0.06), pos_hint={'x': 0.12, 'y': 0.61})
 
-textbox = TextInput(text="First instructions...", font_size=15, size_hint=(0.45, 0.8), pos_hint={'x': 0.5, 'y': 0.1})
+textbox = TextInput(text="Add devices and connect them to HUB by one click.\nNote: Only one HUB is supported",
+          font_size=15, size_hint=(0.45, 0.8), pos_hint={'x': 0.5, 'y': 0.1})
 
 type_label = Label(text="Type Name", font_size=15, size_hint=(0.05, 0.05), pos_hint={'x': 0.03, 'y': 0.53})
 type_spinner = Spinner(text='Select type', size_hint=(0.35, 0.05), pos_hint={'x': 0.12, 'y': 0.53})
@@ -33,7 +35,11 @@ device_text = TextInput(multiline=False, font_size=12, size_hint=(0.2, 0.05), po
 ble_label = Label(text="Device ID:", font_size=15, size_hint=(0.05, 0.05), pos_hint={'x': 0.03, 'y': 0.37})
 ble_text = TextInput(multiline=False, font_size=12, size_hint=(0.2, 0.05), pos_hint={'x': 0.12, 'y': 0.37})
 
-button2 = Button(text='Add Sensor', font_size=15, size_hint=(0.12, 0.06), pos_hint={'x': 0.12, 'y': 0.29})
+checkbox_label = Label(text="", font_size=15, size_hint=(0.2, 0.05), pos_hint={'x': 0.03, 'y': 0.29})
+checkbox = CheckBox(size_hint=(0.05, 0.05), pos_hint={'x': 0.25, 'y': 0.29})
+
+button2 = Button(text='Add Sensor', font_size=15, size_hint=(0.12, 0.06), pos_hint={'x': 0.12, 'y': 0.21})
+
 
 layout.add_widget(url_label)
 layout.add_widget(url_text)
@@ -46,23 +52,28 @@ layout.add_widget(textbox)
 
 # GLOBAL VARIABLES
 types_id_list = []
-
+hub_id = False
 
 # FUNCTIONS
 
-def get_types_pressed(instance):
-    if login_text.text == "" or pass_text.text == "":
-        content_button = Button(text='OK', height=50)
-        popup = Popup(title="Enter login and password!", content=content_button, size_hint=(None, None), size=(250, 200))
-        content_button.bind(on_press=popup.dismiss)
-        popup.open()
-    else:
-        global types_id_list
-        response = api_requests.get_types(url_text.text, login_text.text, pass_text.text)
-        if response[1].status_code == 200:
 
-            types_id_list = response[0]
-            textbox.text = "Get types response HTTP Code: " + str(response[1].status_code)
+def display_popup(message):
+    content_button = Button(text='OK', height=50)
+    popup = Popup(title=message, content=content_button, size_hint=(None, None), size=(250, 200))
+    content_button.bind(on_press=popup.dismiss)
+    popup.open()
+
+
+def get_types_pressed(instance):
+    if login_text.text == "" or pass_text.text == "" or url_text.text == "":
+        display_popup("Enter url password and login!")
+    else:
+        get_types_resp = api_requests.get_types(url_text.text, login_text.text, pass_text.text)
+        if get_types_resp[1].status_code == 200:
+
+            global types_id_list
+            types_id_list = get_types_resp[0]
+            textbox.text = "Get types response HTTP Code: " + str(get_types_resp[1].status_code)
             types_names = []
 
             for entry in types_id_list:
@@ -76,32 +87,74 @@ def get_types_pressed(instance):
             layout.add_widget(ble_label)
             layout.add_widget(ble_text)
             layout.add_widget(button2)
+            layout.add_widget(checkbox_label)
+            layout.add_widget(checkbox)
+
+            global hub_id
+            hub_id = api_requests.get_hub_id(url_text.text, login_text.text, pass_text.text)
+            if bool(hub_id):
+                checkbox_label.text = "HUB ACTIVE. Add to HUB"
+            else:
+                checkbox_label.text = "HUB not present!"
+                checkbox.disabled = True
+
             instance.disabled = True
+
         else:
-            textbox.text = "Problem with getting types [HTTP ERROR]: " + str(response[1].status_code)
+            textbox.text = "Problem with getting types [HTTP ERROR]: " + str(get_types_resp[1].status_code)
+
+
+def spinner_clicked(instance, text):
+    if text == "ACTIVE_HUB":
+        ble_text.disabled = True
+    else:
+        ble_text.disabled = False
 
 
 def add_device_action(instance):
-    if device_text.text == "" or ble_text.text == "" or type_spinner.text == "Select type":
-        content_button = Button(text='OK', height=50)
-        popup = Popup(title="Enter device type, name and id!", content=content_button, size_hint=(None, None), size=(250, 200))
-        content_button.bind(on_press=popup.dismiss)
-        popup.open()
-    else:
-        type_name = type_spinner.text
-        for entry in types_id_list:
-            if entry['name'] == type_name:
-                type_id = entry['id']
-        device_name = device_text.text
-        device_id = ble_text.text
 
-        post_response = api_requests.add_sensor(url_text.text, login_text.text, pass_text.text, type_id, type_name, device_name, device_id)
-        textbox.text = "SENT QUERY: \n" + str(post_response[0]) + "\nSERVER REPLY: " + post_response[1] + "\nADD TO PORTAL: " + post_response[2]
+    if type_spinner.text == "ACTIVE_HUB":
+        if hub_id is False:
+            if device_text.text == "":
+                display_popup("Enter Hub Name!")
+            else:
+                post_response_hub = api_requests.add_hub(url_text.text, login_text.text, pass_text.text, device_text.text)
+                textbox.text = "SENT QUERY: \n" + str(post_response_hub[0]) + "\nSERVER REPLY: " + str(post_response_hub[1]) + "\nADD TO PORTAL: " + str(post_response_hub[2])
+                if post_response_hub[1].status_code == 200 and post_response_hub[2].status_code == 200:
+                    checkbox.disabled = False
+                    checkbox_label.text = "Add to HUB"
+        else:
+            display_popup("One HUB already exists on server!")
+
+    else:
+        if device_text.text == "" or ble_text.text == "" or type_spinner.text == "Select type":
+            display_popup("Enter device type, name and id!")
+
+        else:
+            type_name = type_spinner.text
+            for entry in types_id_list:
+                if entry['name'] == type_name:
+                    type_id = entry['id']
+            device_name = device_text.text
+            device_id = ble_text.text
+
+            post_response = api_requests.add_sensor(url_text.text, login_text.text, pass_text.text, type_id, type_name, device_name, device_id)
+
+            if checkbox.active is True:
+                added_device_id = api_requests.get_device_id(url_text.text, login_text.text, pass_text.text, post_response[3])
+                print(post_response[3], added_device_id)
+                add_to_hub_resp = api_requests.add_to_hub(url_text.text, login_text.text, pass_text.text, hub_id, added_device_id)
+                textbox.text = "SENT QUERY: \n" + str(post_response[0]) + "\nSERVER REPLY: " + str(post_response[1]) + "\nADD TO PORTAL: "\
+                               + str(post_response[2]) + "\nADDED TO HUB: " + str(add_to_hub_resp)
+            else:
+                textbox.text = "SENT QUERY: \n" + str(post_response[0]) + "\nSERVER REPLY: " + str(post_response[1]) + "\nADD TO PORTAL: "\
+                               + str(post_response[2])
 
 # ACTIONS
 
 button1.bind(on_press=get_types_pressed)
 button2.bind(on_press=add_device_action)
+type_spinner.bind(text=spinner_clicked)
 
 
 # MAIN CLASS
